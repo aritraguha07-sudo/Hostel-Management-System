01cc7a1ce1b01ec1c6ced3620e5f6c20054c89a0
Aritra Guha <aritraguha07@gmail.com>
2026-05-07 16:26:26 +0530

    Initial Commit - Hostel Management System

--- src/main/java/com/hostel/hostelweb/controller/RoomController.java
+++ src/main/java/com/hostel/hostelweb/controller/RoomController.java
@@ -6,7 +6,7 @@ import com.hostel.hostelweb.model.Room;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/rooms")
 public class RoomController {
